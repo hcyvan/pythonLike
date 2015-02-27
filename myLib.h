fb1664cfa2576a393c20358434f7098407c4c596
hcyvan <navych@126.com>
2015-02-27 21:14:15 +0800

    Update myLib.h
    Add ERRORMESSAGE which show where the error happened

--- myLib.h
+++ myLib.h
@@ -1,5 +1,6 @@
 #ifndef MYLIB_H
 #define MYLIB_H
+#define ERRORMESSAGE std::cerr<<"Error:"<<__FILE__<<" : in function "<<__func__<<" at line "<<__LINE__<<std::endl
 
 void argcTest(const int argc, const int youNeed);
 
