2280afb711682043d6dffd741057f59cbe5e6a1c
hcyvan <navych@126.com>
2015-03-02 01:56:13 +0800

    Update myLib.h
    add a function 'show' which print the elements of a container.
    The declaration and definition will be separated later...

--- myLib.h
+++ myLib.h
@@ -1,7 +1,16 @@
 #ifndef MYLIB_H
 #define MYLIB_H
+#include <iostream>
 #define ERRORMESSAGE std::cerr<<"Error:"<<__FILE__<<" : in function "<<__func__<<" at line "<<__LINE__<<std::endl
-
+//check the argc of main
 void argcTest(const int argc, const int youNeed);
-
+//print the elements of a container
+template<typename T>
+void show(const T &container)
+{
+	for(auto element:container){
+		std::cout << element <<" ";
+	}
+	std::cout << std::endl;
+}
 #end
