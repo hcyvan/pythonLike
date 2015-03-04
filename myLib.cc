f6e32e9ac137f67b7509c7857d0de503513bcccc
Cheng Yihang <navych@126.com>
2015-03-05 00:51:56 +0800

    Update and rename myLib to myLib.cc
    Remind the number of argument(s) you need in the main program

--- myLib.cc
+++ myLib.cc
@@ -1,7 +1,9 @@
 #include "myLib.h"
 #include <stdexcept>
+#include <iostream>
 void argcTest(const int argc, const int youNeed)
 {
 	if(argc != youNeed){
+		std::cout << "You should enter "<<(youNeed-1)<<(youNeed>2?" arguments.":" argument.")<<std::endl;
 		throw std::runtime_error("Worng argc!!!");
 	}
