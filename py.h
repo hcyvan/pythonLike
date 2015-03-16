dd54a4f11a953d6defc9d480a7fc785b3d772a04
Cheng Yihang <navych@126.com>
2015-03-17 03:01:38 +0800

    Update py.h
    I forgot to add num2string  to py.h before ,now i add it

--- py.h
+++ py.h
@@ -2,12 +2,18 @@
 #define PY_H
 #include <sstream>
 //setpy() :from python's set()
-template<typename T>
-T setpy( T container)
+template<typename T> T setpy( T container)
 {
 	sort(container.begin(),container.end());
 	auto uniqueSet = unique(container.begin(),container.end());
 	container.erase(uniqueSet,container.end());
 	return container;
+}
+//turn number to string
+template<typename T> std::string num2string(const T& i)
+{
+	std::ostringstream out;
+	out << i;
+	return out.str();
 }
 #endif
