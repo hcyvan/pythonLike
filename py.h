685ac8894cb36736c00833909fcdda09c6f8acb7
Cheng Yihang <navych@126.com>
2015-03-20 23:48:53 +0800

    Update py.h

--- py.h
+++ py.h
@@ -2,7 +2,8 @@
 #define PY_H
 #include <sstream>
 //setpy() :from python's set()
-template<typename T> T setpy( T container)
+template<typename T>
+T setpy( T container)
 {
 	sort(container.begin(),container.end());
 	auto uniqueSet = unique(container.begin(),container.end());
